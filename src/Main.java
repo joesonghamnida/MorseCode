3867a02b09253d712cfa880dc07c17bef2ea4d64
Joe Morris <joe.morris.dev@gmail.com>
2017-01-27 02:39:08 -0600

    working

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,20 @@
 import java.util.HashMap;
 import java.util.Scanner;
 
-/**
- * Created by joe on 1/26/17.
- */
 public class Main {
 
     public static HashMap<String, String> letterToCode = new HashMap<>();
 
     public static Scanner scanner = new Scanner(System.in);
 
-    public static void main(String[] args) {
+    public static void main(String[] args)throws Exception {
         mapLettersToCode(letterToCode);
+
+        String morseCode = printCode();
+        codeToSound(morseCode);
+    }
+
+    public static String printCode(){
 
         System.out.println("please enter your message: ");
         String message = scanner.nextLine();
@@ -25,6 +28,33 @@ public class Main {
             }
         }
         System.out.println(morseCode);
+        return morseCode;
+    }
+
+    public static void codeToSound(String morseCode)throws Exception{
+        for(int i = 0; i < morseCode.length(); i++) {
+            int time = 0;
+            String soundClip = "";
+            if(morseCode.charAt(i) == '.'){
+                time = 10;
+                soundClip = "e.wav";
+            }
+            else if(morseCode.charAt(i) == '-'){
+                time = 50;
+                soundClip = "iineShort.wav";
+            }
+
+            long t = System.currentTimeMillis();
+            long end = t + time;
+
+            while (System.currentTimeMillis() < end) {
+                Sounds.playSound(soundClip);
+                Thread.interrupted();
+                Thread.yield();
+            }
+            Thread.sleep(1750);
+        }
+        System.out.println("transmission over");
     }
 
     public static void mapLettersToCode(HashMap<String, String> letterToCode) {
---
+++ src/Sounds.java
@@ -0,0 +1,28 @@
+import sun.audio.AudioPlayer;
+import sun.audio.AudioStream;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+/**
+ * A simple Java sound file example (i.e., Java code to play a sound file).
+ * AudioStream and AudioPlayer code comes from a javaworld.com example.
+ *
+ * @author alvin alexander, devdaily.com.
+ */
+public class Sounds {
+
+    public static void playSound(String soundClip) throws Exception {
+        // open the sound file as a Java input stream
+        String soundFile = "/Users/joe/programming/java/MorseCode/"+soundClip;
+        InputStream in = new FileInputStream(soundFile);
+
+        // create an audiostream from the inputstream
+        AudioStream audioStream = new AudioStream(in);
+
+        // play the audio clip with the audioplayer class
+        AudioPlayer.player.start(audioStream);
+    }
+}
+
+
